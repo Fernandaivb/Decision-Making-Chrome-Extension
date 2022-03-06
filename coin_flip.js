785655b4b1b1317abe6d1aad057ff34950b4671d
Aidan Rutherford <62723997+aidanTheCoder@users.noreply.github.com>
2022-03-06 12:36:55 -0700

    Add images and number gen

--- coin_flip.js
+++ coin_flip.js
@@ -1,3 +1,10 @@
-function wierd() {
-	alert("You called function wierd");
+function coinFlip() {
+    document.getElementById("coin_flip").innerHTML
+        = "<img src = 'images/coin_flip.gif'>";
+    if (Math.floor(Math.random()))
+	document.getElementById("coin_flip").innerHTML 
+	    = "<img src = 'images/coin_heads.jpg'>";
+    else
+        document.getElementById("coin_flip").innerHTML
+	    = "<img src = 'images/coin_tails.jpg'>";
 }
