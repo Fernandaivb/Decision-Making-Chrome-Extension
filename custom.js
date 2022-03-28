9e03d8aeea681fa16537ba851f5132da00807519
Fernanda Benitez <61476818+Fernandaivb@users.noreply.github.com>
2022-03-28 14:27:35 -0700

    Update custom.js

--- custom.js
+++ custom.js
@@ -1,1 +1,10 @@
-//code for the thing
+window.addEventListener("load", function load(event) {
+	var element = document.getElementById("randomize");
+    element.addEventListener("click", function() {
+		print();
+    });
+});
+
+function print() {
+    document.getElementById("result").innerHTML = document.getElementById('option1').value;
+}
