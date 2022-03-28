1a2353f567192ee5f4d833b84ecec939847d2307
Fernanda Benitez <61476818+Fernandaivb@users.noreply.github.com>
2022-03-28 16:58:48 -0700

    Added Randomizer Function

--- custom.js
+++ custom.js
@@ -1,10 +1,21 @@
+function randomizer() {
+    if ((Math.floor(Math.random() * 4) + 1) == 1) {
+        document.getElementById("result").innerHTML = document.getElementById('option1').value;
+      }
+      else if ((Math.floor(Math.random() * 4) + 1) == 2) {
+        document.getElementById("result").innerHTML = document.getElementById('option2').value;
+      }
+      else if ((Math.floor(Math.random() * 4) + 1) == 3) {
+        document.getElementById("result").innerHTML = document.getElementById('option3').value;
+      }
+      else {
+        document.getElementById("result").innerHTML = document.getElementById('option4').value;
+      }
+}
+
 window.addEventListener("load", function load(event) {
 	var element = document.getElementById("randomize");
     element.addEventListener("click", function() {
-		print();
+		randomizer();
     });
 });
-
-function print() {
-    document.getElementById("result").innerHTML = document.getElementById('option1').value;
-}
