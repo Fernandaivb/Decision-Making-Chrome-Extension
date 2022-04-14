ab070cf122bafbea7c39b27f3b7a908227375225
Aidan Rutherford <62723997+aidanTheCoder@users.noreply.github.com>
2022-04-14 15:17:12 -0700

    Updated coin flip
    1) Changed sizes of coin picture
    2) Now have a quarter instead of a penny
    3) Found a better coin gif to spin and now works on 3 second timer
    4) Added a fancy title
    5) Aligned the buttons

--- coin_flip.js
+++ coin_flip.js
@@ -1,15 +1,23 @@
 function coinFlip() {
+	document.getElementById("coin").style.width = "75%";
+	document.getElementById("coin").style.height = "75%";
     if (Math.round(Math.random())) {
 		document.getElementById("coin").src = "images/coin_heads.jpg";
 	} else {
         document.getElementById("coin").src = "images/coin_tails.jpg";
 	}
+	setTimeout(function() {
+		document.getElementById("coin").style.width = "100%";
+		document.getElementById("coin").style.height = "100%";
+		document.getElementById("coin").src = "images/coin_flip_2.gif";
+	}, 3000);
 }
 
 window.addEventListener("load", function load(event) {
 	var tryAgain = document.getElementById("try_again");
 	tryAgain.addEventListener("click", function() {
 		coinFlip();
+		
 	});
 });
 
