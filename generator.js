9ad9a46805c2099cbfb6c6db76926afc3969d648
VanHilstJohann <84753317+VanHilstJohann@users.noreply.github.com>
2022-04-28 22:54:34 -0700

    fixed spacing & fixed NaN message

--- generator.js
+++ generator.js
@@ -2,9 +2,16 @@ function randomNumber() {
 	var min = parseInt(document.getElementsByClassName('min')[0].value);
 	var max = parseInt(document.getElementsByClassName('max')[0].value);
 	
+	
+
 	var result = Math.floor(Math.random() * (max - min + 1)) + min;
 	
-	document.getElementById('result').innerHTML = result;
+	if ((isNaN(min)) || (isNaN(max)) || (isNaN(result))) {
+		document.getElementById('result').innerHTML = "Try again";
+	}
+	else{
+		document.getElementById('result').innerHTML = result;
+	}
 }
 
 window.addEventListener("load", function load(event) {
@@ -13,13 +20,3 @@ window.addEventListener("load", function load(event) {
 		randomNumber();
 	});
 });
-
-function customErrorMsg() {
-	var error = document.getElementById("error");
-	if (isNaN(document.getElementById("min").value)) {
-		error.textContent = "Please enter a number";
-		error.style.color = "red"
-	} else {
-		error.textContent = "";
-	}
-}
